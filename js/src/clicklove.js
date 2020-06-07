ab8a047600d117033fe45406a0d90ef8adcd789f
joe <2904264730@qq.com>
2020-06-07 18:17:44 +0800

    Site updated: 2020-06-07 18:17:42

--- js/src/clicklove.js
+++ js/src/clicklove.js
@@ -47,7 +47,7 @@
 var a_idx = 0;
 jQuery(document).ready(function ($) {
   $("body").click(function (e) {
-    var a = new Array("我", "已", "经", "完", "全", "爱", "上", "warma", "了");
+    var a = new Array("喵", "喵", "喵", "喵", "喵", "喵", "喵", "喵");
     var $i = $("<span/>").text(a[a_idx]);
     a_idx = (a_idx + 1) % a.length;
     var x = e.pageX,
